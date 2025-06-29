029edb3566a2a9e3db3f45bcf98fdedeaf649a08
thanglao <tn2004ttr@gmail.com>
2025-06-29 20:32:53 +0700

    Initial commit

---
+++ DoAn8/menuadmin.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace DoAn8
+{
+    partial class menuadmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menuadmin));
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            btnKho = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnGoiMon = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnQuanLy = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnThongKe = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnTrangChu = new Guna.UI2.WinForms.Guna2GradientButton();
+            pictureBox1 = new PictureBox();
+            guna2GradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.Controls.Add(btnKho);
+            guna2GradientPanel1.Controls.Add(btnGoiMon);
+            guna2GradientPanel1.Controls.Add(btnQuanLy);
+            guna2GradientPanel1.Controls.Add(btnThongKe);
+            guna2GradientPanel1.Controls.Add(btnTrangChu);
+            guna2GradientPanel1.CustomizableEdges = customizableEdges23;
+            guna2GradientPanel1.Location = new Point(1, 1);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            guna2GradientPanel1.Size = new Size(1408, 71);
+            guna2GradientPanel1.TabIndex = 0;
+            // 
+            // btnKho
+            // 
+            btnKho.CustomizableEdges = customizableEdges13;
+            btnKho.DisabledState.BorderColor = Color.DarkGray;
+            btnKho.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnKho.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnKho.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnKho.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnKho.FillColor = Color.FromArgb(108, 61, 40);
+            btnKho.FillColor2 = Color.FromArgb(108, 61, 40);
+            btnKho.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnKho.ForeColor = Color.Black;
+            btnKho.Location = new Point(921, 3);
+            btnKho.Name = "btnKho";
+            btnKho.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            btnKho.Size = new Size(245, 65);
+            btnKho.TabIndex = 2;
+            btnKho.Text = "Quản Lý Kho Nguyên Liệu";
+            // 
+            // btnGoiMon
+            // 
+            btnGoiMon.CustomizableEdges = customizableEdges15;
+            btnGoiMon.DisabledState.BorderColor = Color.DarkGray;
+            btnGoiMon.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnGoiMon.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnGoiMon.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnGoiMon.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnGoiMon.FillColor = Color.FromArgb(108, 61, 40);
+            btnGoiMon.FillColor2 = Color.FromArgb(108, 61, 40);
+            btnGoiMon.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGoiMon.ForeColor = Color.Black;
+            btnGoiMon.Location = new Point(694, 3);
+            btnGoiMon.Name = "btnGoiMon";
+            btnGoiMon.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            btnGoiMon.Size = new Size(230, 65);
+            btnGoiMon.TabIndex = 2;
+            btnGoiMon.Text = "Gọi Món Và Thanh Toán";
+            btnGoiMon.Click += btnGoiMon_Click;
+            // 
+            // btnQuanLy
+            // 
+            btnQuanLy.CustomizableEdges = customizableEdges17;
+            btnQuanLy.DisabledState.BorderColor = Color.DarkGray;
+            btnQuanLy.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnQuanLy.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnQuanLy.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnQuanLy.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnQuanLy.FillColor = Color.FromArgb(108, 61, 40);
+            btnQuanLy.FillColor2 = Color.FromArgb(108, 61, 40);
+            btnQuanLy.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnQuanLy.ForeColor = Color.Black;
+            btnQuanLy.Location = new Point(464, 3);
+            btnQuanLy.Name = "btnQuanLy";
+            btnQuanLy.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            btnQuanLy.Size = new Size(236, 65);
+            btnQuanLy.TabIndex = 1;
+            btnQuanLy.Text = "Quản Lý Nhân Sự & Hệ Thống";
+            // 
+            // btnThongKe
+            // 
+            btnThongKe.CustomizableEdges = customizableEdges19;
+            btnThongKe.DisabledState.BorderColor = Color.DarkGray;
+            btnThongKe.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnThongKe.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnThongKe.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnThongKe.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnThongKe.FillColor = Color.FromArgb(108, 61, 40);
+            btnThongKe.FillColor2 = Color.FromArgb(108, 61, 40);
+            btnThongKe.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnThongKe.ForeColor = Color.Black;
+            btnThongKe.Location = new Point(221, 3);
+            btnThongKe.Name = "btnThongKe";
+            btnThongKe.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            btnThongKe.Size = new Size(237, 65);
+            btnThongKe.TabIndex = 1;
+            btnThongKe.Text = "Thống Kê";
+            // 
+            // btnTrangChu
+            // 
+            btnTrangChu.BackColor = Color.FromArgb(108, 61, 40);
+            btnTrangChu.CustomizableEdges = customizableEdges21;
+            btnTrangChu.DisabledState.BorderColor = Color.DarkGray;
+            btnTrangChu.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnTrangChu.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnTrangChu.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnTrangChu.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnTrangChu.FillColor = Color.FromArgb(108, 61, 40);
+            btnTrangChu.FillColor2 = Color.FromArgb(108, 61, 40);
+            btnTrangChu.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTrangChu.ForeColor = Color.Black;
+            btnTrangChu.Location = new Point(3, 3);
+            btnTrangChu.Name = "btnTrangChu";
+            btnTrangChu.ShadowDecoration.CustomizableEdges = customizableEdges22;
+            btnTrangChu.Size = new Size(231, 65);
+            btnTrangChu.TabIndex = 0;
+            btnTrangChu.Text = "Trang Chủ";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-260, -41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1666, 901);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // menuadmin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(108, 61, 40);
+            ClientSize = new Size(1170, 676);
+            Controls.Add(guna2GradientPanel1);
+            Controls.Add(pictureBox1);
+            Name = "menuadmin";
+            Text = "menuadmin";
+            guna2GradientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Guna.UI2.WinForms.Guna2GradientButton btnKho;
+        private Guna.UI2.WinForms.Guna2GradientButton btnGoiMon;
+        private Guna.UI2.WinForms.Guna2GradientButton btnQuanLy;
+        private Guna.UI2.WinForms.Guna2GradientButton btnThongKe;
+        private Guna.UI2.WinForms.Guna2GradientButton btnTrangChu;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ DoAn8/menuadmin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn8
+{
+    public partial class menuadmin : Form
+    {
+        public menuadmin()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ContextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnGoiMon_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
